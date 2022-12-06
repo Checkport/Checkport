2d9a9b0e92018ddf6e4f1800e02b54effc86dfb6
Zachrobdev <89164169+Zachrobdev@users.noreply.github.com>
2022-12-05 21:41:55 -0600

    added createchecklist infrastructure

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
@@ -7,6 +9,12 @@ export class InfrastructureStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new cdk.aws_lambda.Function(this, "createchecklist", {
+      functionName: "createchecklist",
+      runtime: lambda.Runtime.NODEJS_14_X, // So we can use async in widget.js
+      code: lambda.Code.fromAsset("resources"),
+      handler: "createChecklist.lambdaHandler"
+    })
 
     // example resource
     // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
